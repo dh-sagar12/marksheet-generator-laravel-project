d097154da7f0b0108baf36b5a0716ab5ff020caa
Sagar Dhakal <dhakalsagar2000@gmail.com>
2024-03-21 16:31:23 +0545

    formatting and lintinig

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,34 +1,34 @@
 <div class="flex ">
     <aside class="w-60 h-screen bg-white dark:bg-gray-800 border border-gray-100 dark:border-gray-700 text-gray-900 dark:text-gray-100">
 
-        <ul class=" py-5">
+    <ul class=" py-5">
 
-            <a href="/">
-                <li class="text-xl p-3 my-2 w-full  space-x-2 font-semibold hover:bg-gray-700 ">
-                    <i class="fa-solid fa-gauge"></i>
-                    <span> Dashboard </span>
-                </li>
-            </a>
+        <a href="/">
+            <li class="text-xl p-3 my-2 w-full  space-x-2 font-semibold hover:bg-gray-700 ">
+                <i class="fa-solid fa-gauge"></i>
+                <span> Dashboard </span>
+            </li>
+        </a>
 
-            <a href="{{ route('student.all') }}">
+        <a href="{{ route('student.all') }}">
 
-                <li class="text-xl p-3 my-2 w-full space-x-2 font-semibold hover:bg-gray-700">
-                    <i class="fa-solid fa-users"></i>
-                    <span>Student</span>
-                </li>
-            </a>
+            <li class="text-xl p-3 my-2 w-full space-x-2 font-semibold hover:bg-gray-700">
+                <i class="fa-solid fa-users"></i>
+                <span>Student</span>
+            </li>
+        </a>
 
 
 
-            <a href="{{ route('marksheet.all') }}">
+        <a href="{{ route('marksheet.all') }}">
 
-                <li class="text-xl p-3 my-2 w-full space-x-2 font-semibold hover:bg-gray-700">
-                    <i class="fa-solid fa-sheet-plastic"></i>
-                    <span>Marksheets</span>
-                </li>
-            </a>
+            <li class="text-xl p-3 my-2 w-full space-x-2 font-semibold hover:bg-gray-700">
+                <i class="fa-solid fa-sheet-plastic"></i>
+                <span>Marksheets</span>
+            </li>
+        </a>
 
-        </ul>
+    </ul>
 
-    </aside>
+</aside>
 </div>
